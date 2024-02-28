b349899ddce73ffa9213d6de7c16809b90b22c5f
bk <bo8902@gmail.com>
2024-02-28 13:44:56 +0900

    학교공부 1,4번 페이지 반응형 구현

---
+++ js/school4.js
@@ -0,0 +1,13 @@
+"use strict"
+
+let listicon = document.querySelectorAll('.tabNav li');
+
+listicon.forEach(item => {
+    item.addEventListener('mouseover', function() {
+        item.classList.add('on');
+    });
+
+    item.addEventListener('mouseout', function() {
+        item.classList.remove('on');
+    });
+});
