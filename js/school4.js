69221494c3daab93a7459dd72c64043fd78666f2
bk <bo8902@gmail.com>
2024-02-29 16:27:59 +0900

    스크롤 트랜스폼 추가

--- js/school4.js
+++ js/school4.js
@@ -1,13 +1,42 @@
 "use strict"
 
+// 내부 메뉴바 마우스 오버
 let listicon = document.querySelectorAll('.tabNav li');
 
 listicon.forEach(item => {
     item.addEventListener('mouseover', function() {
         item.classList.add('on');
     });
-
     item.addEventListener('mouseout', function() {
         item.classList.remove('on');
     });
+    item.addEventListener('keyup', function() {
+        item.classList.add('on');
+    });
+    item.addEventListener('keydown', function() {
+        item.classList.remove('on');
+    });
 });
+
+
+function isElementUnderBottom(elem, triggerDiff) {
+    const { top } = elem.getBoundingClientRect();
+    const { innerHeight } = window;
+    return top > innerHeight + (triggerDiff || 0);
+}
+function handleScroll() {
+    const elems = document.querySelectorAll(".up_on_scroll");
+
+    elems.forEach(elem => {
+        if (isElementUnderBottom(elem, -550)) {
+            elem.style.opacity = '0.5';
+            elem.style.transform = 'translateY(80px)';
+            elem.classList.remove('active');
+        } else {
+            elem.style.opacity = '1';
+            elem.style.transform = 'translateY(0px)';
+            elem.classList.add('active');
+        }
+    });
+}
+window.addEventListener('scroll', handleScroll);
