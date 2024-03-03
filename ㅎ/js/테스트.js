e795411ade74aff8b84362c35e23e06bddfd7302
user <special_1106@naver.com>
2024-03-03 23:47:04 +0900

    테스트 파일

---
+++ ㅎ/js/테스트.js
@@ -0,0 +1,23 @@
+// const NOW_CLASS = 'nowClass';
+// const oneSlide = document.querySelector('.box_1');
+// const dots = document.querySelector('.dot');
+//
+// function studentSLide() {
+//     const nowSlide = document.querySelector(`.${NOW_CLASS}`);
+//     if (nowSlide) {
+//         nowSlide.classList.remove(NOW_CLASS);
+//         const nextSlid = nowSlide.nextElementSibling;
+//         if(nextSlid) {
+//             nextSlid.classList.add(NOW_CLASS);} else {
+//             firstSlide.classList.add(NOW_CLASS);
+//         }
+//     }else {
+//         oneSlide.classList.add(NOW_CLASS);
+//         console.log('aa')
+//     }
+// }
+//
+//
+//
+// studentSLide();
+// setInterval(studentSLide, 2000);
