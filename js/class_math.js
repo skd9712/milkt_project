5795cfb6db33d8af1613422f0cf8dd381e284cb9
skd9712 <skd9712@gmail.com>
2024-03-03 16:39:30 +0900

    header & footer 파일 분리, 주석 추가

--- js/class_math.js
+++ js/class_math.js
@@ -1,45 +1,10 @@
+// 최상단 이동 버튼(부드럽게 이동)
 const $topbtn = document.querySelector(".btnTop");
 
 $topbtn.onclick = () => {
     window.screenTo({top: 0, behavior: "smooth"});
 }
-
-document.addEventListener("DOMContentLoaded", function () {
-    // 학교 공부 메뉴
-    let schoolMenuParent = document.querySelector('.menu_li:nth-child(2)');
-    let schoolMenu = document.querySelector('.inner_school');
-
-    schoolMenuParent.addEventListener('mouseover', function () {
-        schoolMenu.classList.add('show');
-    });
-
-    schoolMenuParent.addEventListener('mouseout', function () {
-        schoolMenu.classList.remove('show');
-    });
-
-    // 수준별 메뉴
-    let levelMenuParent = document.querySelector('.menu_li:nth-child(3)');
-    let levelMenu = document.querySelector('.inner_level');
-
-    levelMenuParent.addEventListener('mouseover', function () {
-        levelMenu.classList.add('show');
-    });
-
-    levelMenuParent.addEventListener('mouseout', function () {
-        levelMenu.classList.remove('show');
-    });
-
-    // 햄버거 버튼 클릭 시 메뉴 토글
-    let navbarToggleBtn = document.querySelector('.navbar_toggleBtn');
-    let navbarMenu = document.querySelector('.navbar_menu');
-    let icon = document.querySelector('.navbar_icon');
-
-    navbarToggleBtn.addEventListener('click', function () {
-        navbarMenu.classList.toggle('active');
-        icon.classList.toggle('active');
-    });
-});
-
+// 슬라이드 애니메이션
 const slide = document.querySelector('.slide');
 let slideWidth = slide.clientWidth;
 
