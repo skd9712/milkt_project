0b0e2f80ce703dcb0421271ef19b5d8329e12289
user <special_1106@naver.com>
2024-03-03 09:57:41 +0900

    js만 조금 더 수정........ 예정

---
+++ ㅎ/js/header.js
@@ -0,0 +1,58 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // 학교 공부 메뉴
+    let schoolMenuParent = document.querySelector('.menu_li:nth-child(2)');
+    let schoolMenu = document.querySelector('.inner_school');
+
+    schoolMenuParent.addEventListener('mouseover', function () {
+        schoolMenu.classList.add('show');
+    });
+
+    schoolMenuParent.addEventListener('mouseout', function () {
+        schoolMenu.classList.remove('show');
+    });
+
+    // 수준별 메뉴
+    let levelMenuParent = document.querySelector('.menu_li:nth-child(3)');
+    let levelMenu = document.querySelector('.inner_level');
+
+    levelMenuParent.addEventListener('mouseover', function () {
+        levelMenu.classList.add('show');
+    });
+
+    levelMenuParent.addEventListener('mouseout', function () {
+        levelMenu.classList.remove('show');
+    });
+
+    // 햄버거 버튼 클릭 시 메뉴 토글
+    let navbarToggleBtn = document.querySelector('.navbar_toggleBtn');
+    let navbarMenu = document.querySelector('.navbar_menu');
+    let icon = document.querySelector('.navbar_icon');
+
+    navbarToggleBtn.addEventListener('click', function () {
+        navbarMenu.classList.toggle('active');
+        icon.classList.toggle('active');
+    });
+});
+
+// section 41
+const SHOWING_CLASS = 'showing';
+const firstSlide = document.querySelector('.fist_review');
+
+function reviewSlide() {
+    const currentSlide = document.querySelector(`.${SHOWING_CLASS}`);
+    if (currentSlide) {
+        currentSlide.classList.remove(SHOWING_CLASS);
+        const nextSlide = currentSlide.nextElementSibling;
+        if(nextSlide) {
+            nextSlide.classList.add(SHOWING_CLASS);
+        } else {
+            firstSlide.classList.add(SHOWING_CLASS);
+        }
+    } else {
+        firstSlide.classList.add(SHOWING_CLASS);
+    }
+}
+
+reviewSlide();
+setInterval(reviewSlide, 3000)
+//setInterval(reviewSlide, 100);
