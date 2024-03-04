3b1807df13dcab95dd53f1f1048e3eca9041549d
kim <special_1106@naver.com>
2024-03-04 16:21:37 +0900

    늦어서 죄송합니다......................

--- ㅎ/js/section03.js
+++ ㅎ/js/section03.js
@@ -1,39 +1,15 @@
-let slider = document.querySelector('.infinite_loop_slider');
-let list = document.querySelector('.loop_list');
-let items = document.getElementsByClassName('loop_item');
+// 롤링 배너 복제본 생성
+let roller = document.querySelector('.rolling-list');
+roller.id = 'roller1'; // 아이디 부여
 
-let lastSlider = items.length - 1;
-let firstSlider = items[0]
-let nowIndex = 0
+let clone = roller.cloneNode(true)
+// cloneNode : 노드 복제. 기본값은 false. 자식 노드까지 복제를 원하면 true 사용
+clone.id = 'roller2';
+document.querySelector('.wrap').appendChild(clone); // wrap 하위 자식으로 부착
 
-function clone() {
-    //let cloneSlide_first = items[0].cloneNode(true)
-    let cloneSlide_first = items[0]
-    list.append(cloneSlide_first);
-    //list.appendChild(items[0].cloneNode(true)) // list의 가장 뒤에 item[0]에 cloneNode 붙임
-}
-itemsWidth = items[0].offsetWidth //items의 width만큼 이동하기.
+document.querySelector('#roller1').style.left = '0px';
+document.querySelector('#roller2').style.left = document.querySelector('.rolling-list ul').offsetWidth + 'px';
+// offsetWidth : 요소의 크기 확인(margin을 제외한 padding값, border값까지 계산한 값)
 
-let sliderrr = () => {
-    for (let i=0; i<items.length; i++) {
-        items[i].style.transform -= 'translateX(-160px)'
-        items[i].style.animation = 'rolling 33s linear infinite'
-    }
-    clone();
-}
-
-
-// //기본 실행 함수
-let setI = setInterval(sliderrr, 1000)
-
-// 슬라이드에 마우스 올라가면 멈춤
-list.addEventListener("mouseover", () => {
-    clearInterval(setI);
-});
-
-// 슬라이드에서 마우스 내려가면 다시 시작함
-list.addEventListener("mouseout", () => {
-    setI = setInterval(sliderrr, 1000)
-});
-
-
+roller.classList.add('original');
+clone.classList.add('clone');
